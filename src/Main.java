161e04fd71cbaca0368253626f45267044f68da3
SveinnIF <sveinnif@hiof.no>
2022-01-28 14:03:32 +0100

    moved "actions" to StringUtils

--- src/Main.java
+++ src/Main.java
@@ -8,30 +8,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         List<String> list = ReadFile.read("C:\\Users\\svøn\\IdeaProjects\\logiq_coding_task_Sveinn_Frimannsson_2022\\src\\actiontexts.txt");
 
-        for(String elem : list){
+        for (String elem : list) {
             String[] parts = elem.split(":");
             String action = parts[0];
             String text = parts[1];
 
             switch (action) {
                 case "REVERSE":
-                    System.out.println("Text: ["+ text +"] ran Action: [" + action + "] with the result of: [" + reverse_text(text) + "]");
+                    System.out.println("Text: [" + text + "] ran Action: [" + action + "] with the result of: [" + StringUtils.reverse_text(text) + "]");
                     break;
 
                 case "WHITESPACE":
-                    System.out.println("Text: ["+ text +"] ran Action: [" + action + "] with the result of: [" + remove_whitespace(text) + "]");
+                    System.out.println("Text: [" + text + "] ran Action: [" + action + "] with the result of: [" + StringUtils.remove_whitespace(text) + "]");
                     break;
 
                 case "UNIQUE_COUNT":
-                    System.out.println("Text: ["+ text +"] ran Action: [" + action + "] with the result of: [" + unique_count(text) + "]");
+                    System.out.println("Text: [" + text + "] ran Action: [" + action + "] with the result of: [" + StringUtils.unique_count(text) + "]");
                     break;
 
                 case "PALINDROME":
-                    System.out.println("Text: ["+ text +"] ran Action: [" + action + "] with the result of: [" + palindrome(text) + "]");
+                    System.out.println("Text: [" + text + "] ran Action: [" + action + "] with the result of: [" + StringUtils.palindrome(text) + "]");
                     break;
 
                 case "CAPITALIZE":
-                    System.out.println("Text: ["+ text +"] ran Action: [" + action + "] with the result of: [" + capitalize(text) + "]");
+                    System.out.println("Text: [" + text + "] ran Action: [" + action + "] with the result of: [" + StringUtils.capitalize(text) + "]");
                     break;
 
                 default:
@@ -39,47 +39,8 @@ public class Main {
             }
         }
     }
-
-    private static String reverse_text(String text) {
-
-        StringBuilder text_reversed = new StringBuilder();
-
-        text_reversed.append(text);
-
-        text_reversed.reverse();
-
-        return String.valueOf(text_reversed);
-    }
-
-    private static String remove_whitespace(String text) {
-        return text.replaceAll("\\s", "");
-    }
-
-    private static int unique_count(String text) {
-        int unique_character_count = 0;
-        List<Character> visited_characters = new LinkedList<>();
-        for (int i = 0; i < text.length(); i++) {
-            if (!visited_characters.contains(text.charAt(i))) {
-                unique_character_count++;
-                visited_characters.add(text.charAt(i));
-            }
-        }
-        return unique_character_count;
-    }
-
-    private static String palindrome(String text) {
-        String reversed_text = reverse_text(text);
-        if(reversed_text.equals(text)){
-            return "True";
-        }else{
-            return "False";
-        }
-    }
-
-    private static String capitalize(String text){
-        return text.substring(0, 1).toUpperCase() + text.substring(1);
-    }
 }
+
 
 /*
 In Java 12 it is possible to make the switch statement look like this:
---
+++ src/StringUtils.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class StringUtils {
+
+    public static String reverse_text(String text) {
+
+        StringBuilder text_reversed = new StringBuilder();
+
+        text_reversed.append(text);
+
+        text_reversed.reverse();
+
+        return String.valueOf(text_reversed);
+    }
+
+    public static String remove_whitespace(String text) {
+        return text.replaceAll("\\s", "");
+    }
+
+    public static int unique_count(String text) {
+        int unique_character_count = 0;
+        List<Character> visited_characters = new LinkedList<>();
+        for (int i = 0; i < text.length(); i++) {
+            if (!visited_characters.contains(text.charAt(i))) {
+                unique_character_count++;
+                visited_characters.add(text.charAt(i));
+            }
+        }
+        return unique_character_count;
+    }
+
+    public static String palindrome(String text) {
+        String reversed_text = reverse_text(text);
+        if(reversed_text.equals(text)){
+            return "True";
+        }else{
+            return "False";
+        }
+    }
+
+    public static String capitalize(String text){
+        return text.substring(0, 1).toUpperCase() + text.substring(1);
+    }
+}
+
